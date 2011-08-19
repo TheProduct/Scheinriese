6d9882194c76cb2bbd0a61174f12f14d1c0d94c2
Dennis Paul <d3@the-product.org>
2011-08-19 10:45:06 +0200

    added settings

--- src/ScheinrieseApp.cpp
+++ src/ScheinrieseApp.cpp
@@ -1,3 +1,5 @@
+#include "Resources.h"
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
@@ -7,266 +9,274 @@
 #include "SimpleGUI.h"
 #include <GLUT/glut.h>
 
+#include "Tessellator.c"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 using namespace mowa::sgui;
-
-/* ------------------------ */
-
-void CALLBACK tessBeginCB(GLenum which);
-void CALLBACK tessEndCB();
-void CALLBACK tessErrorCB(GLenum errorCode);
-void CALLBACK tessVertexCB(const GLvoid *data);
-void CALLBACK tessVertexCB2(const GLvoid *data);
-void CALLBACK tessCombineCB(const GLdouble newVertex[3], const GLdouble *neighborVertex[4],
-                            const GLfloat neighborWeight[4], GLdouble **outData);
-
-void CALLBACK tessBeginCB(GLenum which)
-{
-	std::cout << "begin" << std::endl;
-    glBegin(which);
-}
-
-
-
-void CALLBACK tessEndCB()
-{
-    glEnd();
-}
-
-
-
-void CALLBACK tessVertexCB(const GLvoid *data)
-{
-    // cast back to double type
-    const GLdouble *ptr = (const GLdouble*)data;
-	
-	std::cout << ptr[0] << std::endl;
-    glVertex3dv(ptr);
-}
-
-void CALLBACK tessErrorCB(GLenum errorCode)
-{
-    const GLubyte *errorStr;
-	
-    errorStr = gluErrorString(errorCode);
-	std::cout << "[ERROR]: " << errorStr << std::endl;
-}
-
-
-GLuint tessellate1(GLdouble theData[], int theSize)
-{
-    //GLuint id = glGenLists(1);  // create a display list
-    //if(!id) return id;          // failed to create a list, return 0
-	
-    GLUtesselator *tess = gluNewTess(); // create a tessellator
-    if(!tess) return 0;  // failed to create tessellation object, return 0
-	
-    // define concave quad data (vertices only)
-    //  0    2
-    //  \ \/ /
-    //   \3 /
-    //    \/
-    //    1
-    
-	// https://devel.nuclex.org/framework/browser/graphics/fonts/Nuclex.Fonts.Content.TrueTypeImporter/trunk/Source/VectorFonts/FreeTypeFontTessellator.cpp
-    
-    // register callback functions
-    gluTessCallback(tess, GLU_TESS_BEGIN, (void (CALLBACK *)())tessBeginCB);
-    gluTessCallback(tess, GLU_TESS_END, (void (CALLBACK *)())tessEndCB);
-    gluTessCallback(tess, GLU_TESS_ERROR, (void (CALLBACK *)())tessErrorCB);
-    gluTessCallback(tess, GLU_TESS_VERTEX, (void (CALLBACK *)())tessVertexCB);
-	
-	gluTessBeginPolygon(tess, 0);                   // with NULL data
-	gluTessBeginContour(tess);
-	for (int i = 0; i < theSize; i+=3) {
-		//GLdouble myData[3] = {theData[i+0],theData[i+1],theData[i+2]};
-		GLdouble myData1[3] = {100,100,100};
-		gluTessVertex(tess, myData1, myData1);
-	}
-	gluTessEndContour(tess);
-    gluTessEndPolygon(tess);
-	
-    // tessellate and compile a concave quad into display list
-    // gluTessVertex() takes 3 params: tess object, pointer to vertex coords,
-    // and pointer to vertex data to be passed to vertex callback.
-    // The second param is used only to perform tessellation, and the third
-    // param is the actual vertex data to draw. It is usually same as the second
-    // param, but It can be more than vertex coord, for example, color, normal
-    // and UV coords which are needed for actual drawing.
-    // Here, we are looking at only vertex coods, so the 2nd and 3rd params are
-    // pointing same address.
-    //glNewList(id, GL_COMPILE);
-	
-    gluDeleteTess(tess);        // delete after tessellation
-	
-    return 0;//id;      // return handle ID of a display list
-}
-
-
-/* ------------------------ */
 
 
 class ScheinrieseApp : public AppBasic {
     
 public:
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event );
+    void keyDown( KeyEvent event );
 	void update();
 	void draw();
     
 private:
-    void findContours(ImageSourceRef theDepthImage);
-
+    void drawDebug();
+    void findContours(gl::Texture theDepthImage);
+    void findBlobs();
+    void handleKinect();
+    bool showDebug( MouseEvent event );
+    
     SimpleGUI* mGui;
-	float threshold;
-	float blur;
+	float mThreshold;
+	float mBlur;
+    float mKinectTilt;
     
     Kinect mKinect;
 	bool hasKinect;
     
-    ImageSourceRef mDepthImage;
 	gl::Texture mDepthTexture;
     gl::Texture mColorTexture;
     gl::Texture mPreviewTexture;
     
 	vector<vector<cv::Point> > mContours;
+    
+    bool mShowDebug;
+    TextureVarControl* mDebugViewColor;
+    TextureVarControl* mDebugViewDepth;
 };
 
 void ScheinrieseApp::setup()
 {   
     // GUI
 	mGui = new SimpleGUI(this);
-	mGui->addParam("Threshold", &threshold, 0, 255, 100);
-	mGui->addParam("Blur", &blur, 0, 20, 10);
+    mGui->addColumn();
+    mGui->addLabel("CONTROLS");
+	mGui->addParam("Threshold", &mThreshold, 0, 255, 127);
+	mGui->addParam("Blur", &mBlur, 1, 20, 1);
+	mGui->addParam("Tilt", &mKinectTilt, -30, 30, 0);
+    mGui->addColumn();
+    mGui->addLabel("DEBUG VIEW");
+    mGui->addParam("Show Debug", &mShowDebug, true);
+//    mGui->addButton("Show Debug")->registerClick(this, &ScheinrieseApp::showDebug);
+    
+    mGui->load(getResourcePath(RES_SETTINGS));
+    mGui->setEnabled(false);
+    
+    mBlur = 1;
+	mThreshold = 127;
+    
+    mShowDebug = true;
     
 	// KINECT
 	hasKinect = false;
-	console() << "### INFO: There are " << Kinect::getNumDevices() << " Kinects connected." << std::endl;
+	console() << "### INFO: There are " << Kinect::getNumDevices() << " Kinects connected." << endl;
 	if (Kinect::getNumDevices() >= 1) {
 		mKinect = Kinect( Kinect::Device() );
-		mKinect.setTilt(10);
+		mKinect.setTilt(mKinectTilt);
 		hasKinect = true;
 	}
-
-	// OPENCV
-	blur = 11;
-	threshold = 0;
 }
 
+
 void ScheinrieseApp::update()
+{
+    handleKinect();
+	
+	if (mDepthTexture) {
+		findContours(mDepthTexture);
+	}
+}
+
+void ScheinrieseApp::handleKinect() 
 {
     if (!hasKinect) {
 		return;
 	}
-
+    
+    if( mKinectTilt != mKinect.getTilt() ) {
+		mKinect.setTilt( mKinectTilt );
+    }  
+    
 	if( mKinect.checkNewDepthFrame() ) {
-		mDepthImage = mKinect.getDepthImage();
-		mDepthTexture = mDepthImage;
-	}
+		mDepthTexture = mKinect.getDepthImage();
+    }
 	
 	if( mKinect.checkNewVideoFrame() ) {
 		mColorTexture = mKinect.getVideoImage();
-	}	
-	
-	if (mDepthImage) {
-		findContours(mDepthImage);
-	}
+    }
+    
+    /* debug view */
+    if (mColorTexture && !mDebugViewColor) {
+        mGui->addLabel("COLOR");
+        mDebugViewColor = mGui->addParam("COLOR", &mColorTexture);
+        mDebugViewColor->var = &mColorTexture;
+        console() << "color" << endl;
+    }
+    
+    if (mDepthTexture && !mDebugViewDepth) {
+        mGui->addLabel("DEPTH");
+        mDebugViewDepth = mGui->addParam("DEPTH", &mDepthTexture);
+        mDebugViewDepth->var = &mDepthTexture;
+        console() << "depth" << endl;
+    }
 }
 
 void ScheinrieseApp::draw()
 {
 	gl::clear( Color( 0, 0, 0 ) ); 
-	
-	
-	// KINECT
-	if (!hasKinect) {
-		return;
-	}
-	glColor3f( 1.0f, 1.0f, 1.0f );
-	gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
-	if( mDepthTexture ) {
-		gl::draw( mDepthTexture );
-	}
-	if( mColorTexture ) {
-		gl::draw( mColorTexture, Vec2i( 640, 0 ) );
-	}
-	
-	// OPENCV
 	gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
 	glColor3f( 1.0f, 1.0f, 1.0f );
-	if( mPreviewTexture ) {
-		gl::draw( mPreviewTexture, Vec2i( 0, 480 ) );
-	}
-	glPushMatrix();
-	glTranslatef(640,480,0);
-	for (int i = 0; i < mContours.size(); i++) {
-		glColor3f(0.0f,1.0f,1.0f);
-		glBegin( GL_LINE_STRIP );
-		for (int j = 0; j < mContours[i].size(); j+=10) {
-			glVertex2f( mContours[i][j].x, mContours[i][j].y);
-		}
-		glEnd();
-	}
-	glPopMatrix();
+	
+    /* debug view */
+    drawDebug();
     
+    //	glPushMatrix();
+    //	for (int i = 0; i < mContours.size(); i++) {
+    //		glColor3f(0.0f,1.0f,1.0f);
+    //		glBegin( GL_LINE_STRIP );
+    //		for (int j = 0; j < mContours[i].size(); j+=10) {
+    //			glVertex2f( mContours[i][j].x, mContours[i][j].y);
+    //		}
+    //		glEnd();
+    //	}
+    //	glPopMatrix();
     
-	//for (int i = 0; i < mContours.size(); i++) {
-	//		vector<p2t::Point*> polyline;
-	//		for (int j = 0; j < mContours[i].size(); j+=1) {
-	//			polyline.push_back(new p2t::Point(mContours[i][j].x, mContours[i][j].y));
-	//		}
-	//		if (polyline.size() > 9) {
-	//		cout << "Number of line points = " << polyline.size() << endl;
-	//		CDT* cdt = new CDT(polyline);
-	//		cdt->Triangulate();
-	//		//vector<p2t::Triangle*> triangles = cdt->GetTriangles();
-	//		//cout << "Number of triangles = " << triangles.size() << endl;
-	//		}
-	//	}
-	
-	gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
-	glPushMatrix();
-	glTranslatef(200,200,0);
-	GLdouble quad1[4*3] = { -100,300,0, 0,0,0, 100,300,0, 0,200,0 };
-	tessellate1(quad1, 12);
-	glPopMatrix();
-	
-	for (int i = 0; i < mContours.size(); i++) {
-		gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
-		glPushMatrix();
-		for (int j = 0; j < mContours[i].size(); j+=1) {
-			
-		}
-        glPopMatrix();
-	}
-	
+    /*
+     //for (int i = 0; i < mContours.size(); i++) {
+     //		vector<p2t::Point*> polyline;
+     //		for (int j = 0; j < mContours[i].size(); j+=1) {
+     //			polyline.push_back(new p2t::Point(mContours[i][j].x, mContours[i][j].y));
+     //		}
+     //		if (polyline.size() > 9) {
+     //		cout << "Number of line points = " << polyline.size() << endl;
+     //		CDT* cdt = new CDT(polyline);
+     //		cdt->Triangulate();
+     //		//vector<p2t::Triangle*> triangles = cdt->GetTriangles();
+     //		//cout << "Number of triangles = " << triangles.size() << endl;
+     //		}
+     //	}
+     
+     gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
+     glPushMatrix();
+     glTranslatef(200,200,0);
+     GLdouble quad1[4*3] = { -100,300,0, 0,0,0, 100,300,0, 0,200,0 };
+     tessellate(quad1, 12);
+     glPopMatrix();
+     
+     for (int i = 0; i < mContours.size(); i++) {
+     gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
+     glPushMatrix();
+     for (int j = 0; j < mContours[i].size(); j+=1) {
+     
+     }
+     glPopMatrix();
+     }
+     */	
 	// GUI
 	mGui->draw();
 }
+
+void ScheinrieseApp::drawDebug()
+{
+    if ( mShowDebug ) {
+        if ( mDepthTexture ) {
+            gl::draw( mDepthTexture );
+        }
+        
+        if ( mColorTexture ) {
+            gl::draw( mColorTexture );
+        }
+        
+        if ( mPreviewTexture ) {
+            gl::draw( mPreviewTexture );
+        }
+    }
+}
+
 
 void ScheinrieseApp::mouseDown( MouseEvent event )
 {
 }
 
-void ScheinrieseApp::findContours(ImageSourceRef theDepthImage) {
+bool ScheinrieseApp::showDebug( MouseEvent event )
+{ 
+    return false;
+}
+
+
+void ScheinrieseApp::keyDown( KeyEvent event )
+{
+    switch(event.getChar()) {				
+        case 'd': mGui->dump(); break;
+        case 'l': mGui->load(getResourcePath(RES_SETTINGS)); break;
+        case 's': mGui->save(getResourcePath(RES_SETTINGS)); break;
+    }
+    switch(event.getCode()) {
+        case KeyEvent::KEY_ESCAPE: quit(); break;
+        case KeyEvent::KEY_SPACE: mGui->setEnabled(!mGui->isEnabled());
+    }
+}
+
+
+void ScheinrieseApp::findContours(gl::Texture theDepthImage)
+{    
+//ImageSourceRef theDepthImage) {
     // images that opencv work on
-	cv::Mat input( toOcv( Channel8u( theDepthImage ) ) );
-	cv::Mat blurred;
-	cv::Mat thresholded;
-	cv::Mat output;
-	
-	cv::blur(input, blurred, cv::Size(blur,blur));
-	cv::threshold( blurred, thresholded, threshold, 255, CV_THRESH_BINARY);
-	
-	// store thresholded image in a texture
-	cv::cvtColor( thresholded, output, CV_GRAY2RGB );
-	mPreviewTexture = gl::Texture( fromOcv(output) );
-	
-	// find and store contours
-	mContours.clear();
-	cv::findContours(thresholded, mContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);	
+    cv::Mat input( toOcv( Channel8u( theDepthImage ) ) );
+    cv::Mat blurred;
+    cv::Mat thresholded;
+    cv::Mat output;
+    
+    cv::blur(input, blurred, cv::Size(mBlur, mBlur));
+    cv::threshold( blurred, thresholded, mThreshold, 255, CV_THRESH_BINARY);
+    
+    // store thresholded image in a texture
+    cv::cvtColor( thresholded, output, CV_GRAY2RGB );
+    mPreviewTexture = gl::Texture( fromOcv(output) );
+    
+    // find and store contours
+    mContours.clear();
+    cv::findContours(thresholded, mContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);	
+}
+
+void ScheinrieseApp::findBlobs() {
+    CBlobResult blobs;
+    int i;
+    CBlob *currentBlob;
+    IplImage *original, *originalThr;
+    
+    // load an image and threshold it
+    original = cvLoadImage("pic1.png", 0);
+    cvThreshold( original, originalThr, 100, 0, 255, CV_THRESH_BINARY );
+    
+    // find non-white blobs in thresholded image
+    blobs = CBlobResult( originalThr, NULL, 255 );
+    // exclude the ones smaller than param2 value
+    blobs.Filter( blobs, B_EXCLUDE, CBlobGetArea(), B_LESS, param2 );
+    
+    // get mean gray color of biggest blob
+    CBlob biggestBlob;
+    CBlobGetMean getMeanColor( original );
+    double meanGray;
+    
+    blobs.GetNth( CBlobGetArea(), 0, biggestBlob );
+    meanGray = getMeanColor( biggestBlob );
+    
+    // display filtered blobs
+    cvMerge( originalThr, originalThr, originalThr, NULL, displayedImage );
+    
+    for (i = 0; i < blobs.GetNumBlobs(); i++ )
+    {
+        currentBlob = blobs.GetBlob(i);
+        currentBlob->FillBlob( displayedImage, CV_RGB(255,0,0));
+    }
 }
 
 CINDER_APP_BASIC( ScheinrieseApp, RendererGl )
